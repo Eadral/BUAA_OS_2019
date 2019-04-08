ff21ae59d1491806a1b57717df4649bddfb65c82
Eadral <a250014238@me.com>
2019-04-08 16:41:43 +0800

    help

--- mm/pmap.c
+++ mm/pmap.c
@@ -693,10 +693,10 @@ u_long cal_page(int taskKind, u_long va, int n, Pde *pgdir) {
         int i = va >> 22;
         //printf("%d\n", i);
         u_long *entry = pgdir + i;
-        u_long add = (va >> 12) * 4 + va;
+        u_long add = cal_page(1, va, 0, 0);
         //printf("%x\n", add);
         //printf("%x\n", PADDR(add));
-        *entry = PADDR(add) | PTE_V | PTE_R;
+        *entry = PADDR(add) | PTE_V;
         //printf("%x\n", *entry);
 
         return 0;
